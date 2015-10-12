eb48790a0758ad8270f810a9f9f7f1872ec02427
Katrina Ezis <katrinaezis@gmail.com>
2015-10-12 02:34:42 -0700

    try

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 "use strict";
 
 window.onload = function() {
-	window.alert("kk");
 	var dawg = document.getElementById("dawg");
 	dawg.onscroll = blur;
 };
 
 function blur() {
+	window.alert("kk");
 	var filter = 'brightness(50%) blur(10px)';
 	dawg.style.webkitFilter = filter;
 	dawg.style.filter = filter;
